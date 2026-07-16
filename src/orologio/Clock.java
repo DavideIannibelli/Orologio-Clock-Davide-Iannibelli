547bf7ce114abf48620243e644170147bca562b7
Davide <1145A@A1145>
2026-07-16 17:19:25 +0200

    aggiornato

--- src/orologio/Clock.java
+++ src/orologio/Clock.java
@@ -14,17 +14,23 @@ public class Clock {
 		ora = scanner.nextInt();
 		System.out.print("Inserisci minuti: ");
 		minuti = scanner.nextInt();
-		
-		
-		if (ora>=6 && ora<=11 && minuti<=59) {
+	
+		if (ora<=-1) {
+		System.out.print("Orario negativo non accettato");
+		} else if (minuti<=-1) {
+			System.out.print("Orario negativo non accettato");
+			}
+		else if (ora>=6 && ora<=11 && minuti<=59) {
 			System.out.print("L'ora" +ora+ ":" +minuti+ " è MATTINA");
 		} else if (ora>=12 && ora<=17 && minuti<=59) {
 			System.out.print("L'ora" +ora+ ":" +minuti+ " è POMERIGGIO");
 		} else if (ora>=18 && ora<=22 && minuti<=59) {
 			System.out.print("L'ora" +ora+ ":" +minuti+ " è SERA");
-		} else if (ora>=23 || ora<=5  && minuti<=59) {
+		} else if (ora>=23 || ora<=5 && minuti<=59) {
 			System.out.print("L'ora" +ora+ ":" +minuti+ " è NOTTE");
-		} 
+		} else if (ora<=-1) {
+			System.out.print("Orario negativo non accettato");
+		}
 		else {
 			System.out.print("Orario Inesistente! Le ore vanno dalle 00 alle 24");
 		}
